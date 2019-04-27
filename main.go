5cc9893ce76375120f930bdb4532d99d0a420d08
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-27 23:51:46 +0900

    optionをloadするやつ

--- main.go
+++ main.go
@@ -25,3 +25,18 @@ func closedb() {
 	db.Close()
 	log.Println("データベース接続を閉じられました")
 }
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
