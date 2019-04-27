9cf6ba5dfd5fe78ed3da2c6c7951d4f53c3ceaf8
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-27 12:56:14 +0900

    mainにmongodbへの接続関連の関数を作る

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
 func main() {
 
+}
+
+var db *mgo.Session
+
+// MongoDBとの接続
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイアル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+// MongoDBとの接続を解除
+func closedb() {
+	db.Close()
+	log.Println("データベース接続を閉じられました")
 }
