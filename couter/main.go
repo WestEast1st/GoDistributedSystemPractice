86a013b08fbb2d691aac71086a318a870aaae8a3
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-28 16:32:01 +0900

    nsqのメッセージ受信

--- couter/main.go
+++ couter/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
+	nsq "github.com/bitly/go-nsq"
 	mgo "gopkg.in/mgo.v2"
 )
 
@@ -18,6 +20,8 @@ func fatal(e error) {
 }
 
 func main() {
+	var countsLook sync.Mutex
+	var counts map[string]int
 	defer func() {
 		if fatalErr != nil {
 			os.Exit(1)
@@ -34,4 +38,27 @@ func main() {
 		db.Close()
 	}()
 	pollData := db.DB("ballots").C("polls")
+	//NSQに接続
+	log.Println("NSQに接続ます")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	//NSQからメッセージを受け取った時の処理
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLook.Lock()
+		defer countsLook.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
